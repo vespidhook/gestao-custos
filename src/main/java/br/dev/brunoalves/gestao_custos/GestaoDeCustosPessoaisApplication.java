97b1c4f0c6c61b0e60bdac920446e9d2848771c3
Bruno Alves <basilva@unisuam.edu.br>
2025-06-13 23:46:17 -0300

    first commit

---
+++ src/main/java/br/dev/brunoalves/gestao_custos/GestaoDeCustosPessoaisApplication.java
@@ -0,0 +1,13 @@
+package br.dev.brunoalves.gestao_custos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeCustosPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeCustosPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/brunoalves/gestao_custos/GestaoDeCustosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.brunoalves.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeCustosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
